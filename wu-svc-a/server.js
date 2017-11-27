ddcce3e115b10bb23b7858bf1cd0ce7cbf97f41a
Vijay Bhaskar Chintalapati <vchintal@intrepid.lan>
2017-11-27 09:27:17 -0800

    Initial commit

---
+++ wu-svc-a/server.js
@@ -0,0 +1,19 @@
+var sleep = require('sleep');
+var express = require('express');
+var app = express();
+var router = express.Router(); 
+var port = process.env.PORT || 8080; 
+
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+router.get('/', function(req, res) {
+    var sleepTime = randomIntFromInterval(10,50);
+    console.log('Sleeping for '+sleepTime+' ms');
+    sleep.msleep(sleepTime);
+    res.send('AUTHORIZED');
+});
+
+app.use('/api', router);
+app.listen(port);
