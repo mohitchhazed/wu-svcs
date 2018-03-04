19c76c269a7578ca8c2bd63c50603aaf109c3a70
mohitchhazed <mohitchhazed@users.noreply.github.com>
2018-03-04 21:40:27 +0530

    Update server.js
    port change

--- wu-svc-a/server.js
+++ wu-svc-a/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-var port = process.env.PORT || 9000;
+var port = process.env.PORT || 8080;
 
 function randomIntFromInterval(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
